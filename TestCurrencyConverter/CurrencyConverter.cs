17f218d821897d473b699a4c26f22767504bfbf4
amir <amirh.esmail.zade@gmail.com>
2022-05-04 17:14:27 +0430

    first commit

---
+++ TestCurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCurrencyConverter
+{
+    public class CurrencyConverter : ICurrencyConverter
+    {
+        public readonly Dictionary<(string, string), double> convertRates;
+
+        public CurrencyConverter()
+        {
+            this.convertRates = new Dictionary<(string, string), double>();
+        }
+
+        public void ClearConfiguration()
+        {
+            convertRates.Clear();
+        }
+
+        public double Convert(string fromCurrency, string toCurrency, double amount)
+        {
+            if(convertRates.ContainsKey((fromCurrency, toCurrency)))    
+                return amount * convertRates[(fromCurrency, toCurrency)];
+
+            if (convertRates.ContainsKey((toCurrency, fromCurrency)))
+                return amount / convertRates[(toCurrency, fromCurrency)];
+
+            throw new Exception("convert rate not found");
+        }
+
+        public void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates)
+        {
+            ClearConfiguration();
+
+            foreach (var conversionRate in conversionRates)
+                AddRelation(conversionRate.Item1, conversionRate.Item2, conversionRate.Item3);
+        }
+
+        public void AddRelation(string from, string to, double rate)
+        {
+            if (convertRates.ContainsKey((from, to)) || convertRates.ContainsKey((to, from)))
+                return;
+
+            convertRates.Add((from, to), rate);
+            ManageRelation(from, to);
+        }
+
+        private void ManageRelation(string first, string second)
+        {
+            var rate = convertRates[(first, second)];
+            var firstCurrencyRelations = convertRates
+                .Where(c => (c.Key.Item1 == first && c.Key.Item2 != second)
+                || (c.Key.Item2 == first && c.Key.Item1 != second)).ToList();
+
+            foreach (var relation in firstCurrencyRelations)
+            {
+                if (relation.Key.Item1 == first)
+                {
+                    var newRate = rate / relation.Value;
+                    AddRelation(relation.Key.Item2, second, newRate);
+                }
+                else // item2 == first
+                {
+                    var newRate = rate * relation.Value;
+                    AddRelation(relation.Key.Item1, second, newRate);
+                }
+            }
+
+            var secondCurrencyRelations = convertRates
+                .Where(c => (c.Key.Item1 == second && c.Key.Item2 != first)
+                || (c.Key.Item2 == second && c.Key.Item1 != first)).ToList();
+
+            foreach (var relation in secondCurrencyRelations)
+            {
+                if (relation.Key.Item1 == second)
+                {
+                    var newRate = rate * relation.Value;
+                    AddRelation(first , relation.Key.Item2, newRate);
+                }
+                else // item2 == second
+                {
+                    var newRate = rate / relation.Value;
+                    AddRelation(relation.Key.Item1, first, newRate);
+                }
+            }
+        }
+    }
+}
---
+++ TestCurrencyConverter/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestCurrencyConverter;
+
+var currencyConverter = new CurrencyConverter();
+
+currencyConverter.AddRelation("cad", "gbp", .8);
+currencyConverter.AddRelation("usd", "cad", 1.3);
+currencyConverter.AddRelation("usd", "eur", 1.2);
+currencyConverter.AddRelation("usd", "ir", .2);
+
+currencyConverter.UpdateConfiguration(new List<Tuple<string, string, double>>()
+{
+    new Tuple<string, string, double>("cad", "gbp", .8),
+    new Tuple<string, string, double>("usd", "cad", 1.3),
+    new Tuple<string, string, double>("usd", "eur", 1.2),
+});
+
+foreach (var item in currencyConverter.convertRates)
+    Console.WriteLine($"{item.Key.Item1} to {item.Key.Item2} : {item.Value}");
+
+Console.WriteLine();
+
+Console.WriteLine(currencyConverter.Convert("gbp", "eur", 3));
+Console.WriteLine(currencyConverter.Convert("eur", "gbp", 3));
+
+Console.Read();
